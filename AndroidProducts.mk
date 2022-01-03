cc4e3cc3fd04d9451dfbe719badbd1bf03bd31e3
Hemker1469 <97006255+Hemker1469@users.noreply.github.com>
2022-01-03 08:26:17 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The SparkOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_pine.mk
+    $(LOCAL_DIR)/lineage_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_pine-user \
-    spark_pine-userdebug \
-    spark_pine-eng
+    lineage_pine-user \
+    lineage_pine-userdebug \
+    lineage_pine-eng
